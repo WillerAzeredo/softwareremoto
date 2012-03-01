8ceddffa019cc33f1caeaf2096c10332bc161dfa
Willer Azeredo <willer_azeredo@hotmail.com>
2012-03-01 17:05:34 -0300

    Criação do Projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Softwareremoto::Application.config.secret_token = '34a6ea03b9e5d123425929e123fdad7d0b71fe679ed213c85f06a5d53c78e716f9c9d73e78bc2bb1bbbe6d512850344fa26b7d308d0459bb44c5c0c4e2e45e79'
